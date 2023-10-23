efab2a876e6842f20130c54f4b179c63fe367c8c
ptanhVNU <ptanh.uet@gmail.com>
2023-10-23 23:56:25 +0700

    impl service

--- app/src/main/java/com/dev/musicplayer/di/ServiceModule.kt
+++ app/src/main/java/com/dev/musicplayer/di/ServiceModule.kt
@@ -3,6 +3,12 @@ package com.dev.musicplayer.di
 import android.content.Context
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.database.StandaloneDatabaseProvider
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.cache.CacheDataSource
+import androidx.media3.datasource.cache.NoOpCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
 import androidx.media3.exoplayer.ExoPlayer
 import dagger.Module
 import dagger.Provides
@@ -10,6 +16,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
+import java.io.File
 
 @Module
 @InstallIn(ServiceComponent::class)
@@ -33,6 +40,36 @@ object ServiceModule {
         /// Sets whether the player should pause automatically
         // when audio is rerouted from a headset to device speakers.
         setHandleAudioBecomingNoisy(true)
+    }
+
+    @Provides
+    @ServiceScoped
+    fun provideDataSourceFactory(
+        @ApplicationContext context: Context
+    ) = DefaultDataSource.Factory(context)
+
+
+    @Provides
+    @ServiceScoped
+    @UnstableApi
+    // Creates a `CacheDataSource.Factory` that uses a `SimpleCache` to cache data from a `DefaultDataSource.Factory`.
+    fun provideCacheDataSourceFactory(
+        @ApplicationContext context: Context,
+        dataSource: DefaultDataSource.Factory
+    ): CacheDataSource.Factory {
+        // Creates a `File` object for the cache directory.
+        val cacheDir = File(context.cacheDir, "media")
+
+        // Creates a `StandaloneDatabaseProvider` object to provide a database connection to the `SimpleCache`.
+        val databaseProvides = StandaloneDatabaseProvider(context)
+
+        // Creates a `SimpleCache` object to cache data.
+        val cache = SimpleCache(cacheDir, NoOpCacheEvictor(), databaseProvides)
+
+        return CacheDataSource.Factory().apply {
+            setCache(cache)
+            setUpstreamDataSourceFactory(dataSource)
+        }
     }
 
 }
--- app/src/main/java/com/dev/musicplayer/services/MusicService.kt
+++ app/src/main/java/com/dev/musicplayer/services/MusicService.kt
@@ -1,20 +1,35 @@
 package com.dev.musicplayer.services
 
+import android.content.Intent
+import android.util.Log
 import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MusicService : MediaSessionService (){
 
-    @Inject
     lateinit var mediaSession: MediaSession
 
+    lateinit var player: ExoPlayer
+
+    //TODO: import data source
+
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.w("Service", "Media Service Created")
+
+
+    }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession = mediaSession
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
     override fun onDestroy() {
         super.onDestroy()
         mediaSession.apply {
