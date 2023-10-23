b74fc48dfd67d9cf9402aa0f1a9df4501b6749dd
weiwei146 <100007345+weiwei146@users.noreply.github.com>
2023-10-24 00:25:08 +0700

    bottom sheets

--- app/src/main/java/com/dev/musicplayer/presentation/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/dev/musicplayer/presentation/playlist/PlaylistScreen.kt
@@ -2,6 +2,7 @@ package com.dev.musicplayer.presentation.playlist
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,32 +11,42 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @Composable
 fun PlusButton(icon: ImageVector, onClick: () -> Unit) {
@@ -66,6 +77,13 @@ fun PlaylistScreen() {
             "Bleach"
         )
     }
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember {
+        mutableStateOf(false)
+    }
+    var textAdd by remember {
+        mutableStateOf("") }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -78,69 +96,124 @@ fun PlaylistScreen() {
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Spacer(modifier = Modifier.width(25.dp))
                 Text(
-                    text = "Playlist",
-                    color = Color.Green,
+                    text = "   Your Library",
+                    color = Color.White,
                     fontSize = 30.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
                 PlusButton(
                     icon = Icons.Default.Add,
-                    onClick = {}
+                    onClick = {
+                        showBottomSheet = true
+                    }
                 )
             }
             Spacer(modifier = Modifier.height(16.dp))
-            SearchBar(
-                modifier = Modifier.fillMaxWidth(),
-                query = text,
-                onQueryChange = {
-                    text = it
-                },
-                onSearch = {
-                    items.add(text)
-                    active = false
-                },
-                active = active,
-                onActiveChange = {
-                    active = it
-                },
-                placeholder = {
-                    Text(text = "Search")
-                },
-                leadingIcon = {
-                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
-                },
-                trailingIcon = {
-                    if(active) {
-                        Icon(
-                            modifier = Modifier.clickable {
-                                if(text.isNotEmpty()) {
-                                    text = ""
-                                } else {
-                                    //ấn close thì list search biến mất
-                                    active = false;
-                                }
-                            },
-                            imageVector =  Icons.Default.Close,
-                            contentDescription = "Close Icon"
-                        )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(max = 200.dp)
+            ) {
+                SearchBar(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(all = 10.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                    query = text,
+                    onQueryChange = {
+                        text = it
+                    },
+                    onSearch = {
+                        items.add(text)
+                        active = false
+                    },
+                    active = active,
+                    onActiveChange = {
+                        active = it
+                    },
+                    placeholder = {
+                        Text(text = "Search")
+                    },
+                    leadingIcon = {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
+                    },
+                    trailingIcon = {
+                        if(active) {
+                            Icon(
+                                modifier = Modifier.clickable {
+                                    if(text.isNotEmpty()) {
+                                        text = ""
+                                    } else {
+                                        //ấn close thì list search biến mất
+                                        active = false;
+                                    }
+                                },
+                                imageVector =  Icons.Default.Close,
+                                contentDescription = "Close Icon"
+                            )
+                        }
+                    }
+                ) {
+                    items.forEach {
+                        Row(
+                            modifier = Modifier.padding(all = 14.dp)
+                        ) {
+                            Icon(
+                                modifier = Modifier.padding(end = 10.dp),
+                                imageVector = Icons.Default.History,
+                                contentDescription = "History Icon"
+                            )
+                            Text(
+                                text = it
+                            )
+                        }
                     }
                 }
+            }
+            Scaffold(
             ) {
-                items.forEach {
-                    Row(
-                        modifier = Modifier.padding(all = 14.dp)
+                contentPadding->Box(
+                    modifier = Modifier.padding(contentPadding)
+                        .background(Color.Black)
+                        .fillMaxSize()
+                )
+                if (showBottomSheet) {
+                    ModalBottomSheet(
+                        onDismissRequest = {
+                            showBottomSheet = false
+                        },
+                        sheetState = sheetState
                     ) {
-                        Icon(
-                            modifier = Modifier.padding(end = 10.dp),
-                            imageVector = Icons.Default.History,
-                            contentDescription = "History Icon"
-                        )
-                        Text(
-                            text = it
-                        )
+                        Column (
+                            modifier = Modifier.fillMaxWidth().padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            TextField(
+                                modifier = Modifier.clip(RoundedCornerShape(15.dp)),
+                                value = textAdd,
+                                onValueChange = {
+                                    textAdd = it
+                                },
+                                placeholder = {
+                                    Text(text = "Name of playlist")
+                                },
+                                label = {
+                                    Text("")
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(15.dp))
+                            Button(
+                                onClick = {
+                                    scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                        if (!sheetState.isVisible) {
+                                            showBottomSheet = false
+                                        }
+                                    }
+                                }
+                            ) {
+                                Text("Submit")
+                            }
+                        }
                     }
                 }
             }
