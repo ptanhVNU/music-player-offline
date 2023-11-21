9bd421be3d9c9d7b04b1c177ab9e270b6e975503
ptanhVNU <ptanh.uet@gmail.com>
2023-11-21 21:53:32 +0700

    fix bugs

---
+++ app/src/main/java/com/dev/musicplayer/core/shared/models/MediaAudioItem.kt
@@ -0,0 +1,34 @@
+package com.dev.musicplayer.core.shared.models
+
+import android.graphics.Bitmap
+import android.net.Uri
+import com.dev.musicplayer.data.local.entities.Song
+
+data class MediaAudioItem(
+    val name: String,
+    val absolutePath: String,
+    val artist: String,
+    val id: Long,
+    val uri: Uri,
+    val size: Long,
+    val duration: Long,
+    val dateModified: Long,
+    val artWork: Bitmap?,
+
+    ) {
+    fun Long.toHhMmSs(): String {
+        val seconds = (this / 1000).toInt()
+        val hours = seconds / 3600
+        val minutes = (seconds % 3600) / 60
+        val remainingSeconds = seconds % 60
+        return String.format("%02d:%02d:%02d", hours, minutes, remainingSeconds)
+    }
+
+    fun MediaAudioItem.toSong() : Song {
+        return Song(
+            songId = id,
+            title = name,
+            uri = uri.toString(),
+        )
+    }
+}
