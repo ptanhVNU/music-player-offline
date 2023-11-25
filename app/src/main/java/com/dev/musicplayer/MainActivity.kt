9b007d87c26a9884a7be18dea89d5eafb0f80bcc
ptanhVNU <ptanh.uet@gmai.com>
2023-11-25 09:55:40 +0700

    adjust permission for api > 32

--- app/src/main/java/com/dev/musicplayer/MainActivity.kt
+++ app/src/main/java/com/dev/musicplayer/MainActivity.kt
@@ -98,7 +98,7 @@ private fun RequestPermissionAndDisplayContent(
 
     val readVideoPermissionState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         rememberPermissionState(
-            Manifest.permission.READ_MEDIA_VIDEO
+            Manifest.permission.READ_MEDIA_AUDIO
         )
     } else {
         rememberPermissionState(
